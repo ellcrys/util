18a4bef857a21d458a8dee275fd7fa14fe9f0b91
Kennedy Idialu <kennedyidialu@gmail.com>
2016-04-03 20:23:46 +0100

    initial commit

---
+++ utility.go
@@ -0,0 +1,341 @@
+package util
+
+import (
+	"fmt"
+    r "math/rand"
+    "errors"
+    "io"
+    "bufio"
+    "time"
+    "encoding/json"
+    "crypto/sha1"
+    "strings"
+    "os"
+    "path/filepath"
+    "strconv"
+    "io/ioutil"
+
+    "github.com/satori/go.uuid"
+)
+
+func init(){
+    r.Seed(time.Now().UnixNano())
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Convenient function for fmt.Println.
+// Useful when you already have util package imported in current file 
+func Println(any... interface{}) {
+	fmt.Println(any...)
+}
+
+// Get UUID v4 string
+func UUID4() string {
+    return uuid.NewV4().String()
+}
+
+// sha1 hashing
+func Sha1(str string) string {
+    h := sha1.New()
+    h.Write([]byte(str))
+    return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// decode json string to map
+func DecodeJSONToMap(str string) (map[string]interface{}, error) {
+    var dat map[string]interface{}
+    if err := json.Unmarshal([]byte(str), &dat); err != nil {
+        return map[string]interface{}{}, err
+    }
+    return dat, nil
+}
+
+// get random string of fixed length
+func RandString(n int) string {
+    b := make([]byte, n)
+    for i := range b {
+        b[i] = letterBytes[r.Intn(len(letterBytes))]
+    }
+    return string(b)
+}
+
+// Read n bytes from a pipe and pass bytes read to a callback. If an error occurs
+// error is passed to the callback. The callback signature is:
+// Func(err error, bs []byte, done bool).
+func ReadReader(reader io.Reader, nBytes int, cb func(err error, bs []byte, done bool) bool )  {
+    r := bufio.NewReader(reader)
+    buf := make([]byte, 0, nBytes)
+    for {
+        n, err := r.Read(buf[:cap(buf)])
+        buf = buf[:n]
+        if n == 0 {
+            if err == nil {
+                continue
+            }
+            if err == io.EOF {
+                break
+            }
+            if cb(err, buf, false) == false {
+                return
+            }
+        }
+  
+        // process buf
+        if err != nil && err != io.EOF {
+            if cb(err, buf, false) == false {
+                return
+            }
+        }
+
+        if cb(err, buf, false) == false {
+            return
+        }
+    }
+
+    cb(nil, buf, true)
+}
+
+// cast to map
+func Map(m interface{}) map[string]interface{} {
+    return m.(map[string]interface{})
+}
+
+// convert stringified JSON array to slice of string.
+// JSON array must contain only string values
+func JSONToSliceString(jsonStr string) ([]string, error) {
+    var data []string
+    d := json.NewDecoder(strings.NewReader(jsonStr))
+    d.UseNumber()
+    if err := d.Decode(&data); err != nil {
+        return []string{}, errors.New("unable to parse json string");
+    }
+    return data, nil
+}
+
+// convert a byte array to string
+func ByteArrToString(byteArr []byte) string {
+    return fmt.Sprintf("%s", byteArr)
+}
+
+// Read files from /tests/fixtures/ directory
+func ReadFromFixtures(path string) string {
+    absPath, _ := filepath.Abs(path)
+    dat, err := ioutil.ReadFile(absPath)
+    if err != nil {
+        panic(err)
+    }
+    return fmt.Sprintf("%s", dat)
+}
+
+// generate random numbers between a range
+func RandNum(min, max int) int {
+    s1 := r.NewSource(time.Now().UnixNano())
+    r1 := r.New(s1)
+    return r1.Intn(max-min) + min
+}
+
+// Generate an id to be used as a stone id.
+func NewID() string {
+    curTime := int(time.Now().Unix())
+    id := fmt.Sprintf("%s:%d", UUID4(), curTime)
+    return Sha1(id)
+}
+
+// Checks that a string slice contains a string value
+func InStringSlice(ss []string, val string) bool {
+    for _, v := range ss {
+        if v == val {
+            return true
+        }
+    }
+    return false
+}
+
+// Checks if a key exists in a map
+func HasKey(m map[string]interface{}, key string) bool {
+    for k, _ := range m {
+        if k == key {
+            return true
+        }
+    }
+    return false
+}
+
+// Checks that a value type is string
+func IsStringValue(any interface{}) bool {
+    switch any.(type) {
+    case string:
+        return true
+    default:
+        return false
+    }
+}
+
+// Get all the keys of a map
+func GetMapKeys(m map[string]interface{}) []string {
+    mk := make([]string, len(m))
+    i := 0
+    for key, _ := range m {
+        mk[i] = key
+        i++
+    }
+    return mk
+}
+
+// checks that a variable value type is a map of any value
+func IsMapOfAny(any interface{}) bool {
+    switch any.(type) {
+    case map[string]interface{}:
+        return true
+        break
+    default:
+        return false
+        break
+    }
+    return false
+}
+
+// checks that a variable value type is a slice
+func IsSlice(any interface{}) bool {
+    switch any.(type) {
+    case []interface{}:
+        return true
+        break
+    default:
+        return false
+        break
+    }
+    return false
+}
+
+// checks that a slice contains map[string]interface{} type
+func ContainsOnlyMapType(s []interface{}) bool {
+    for _, v := range s {
+        switch v.(type) {
+        case map[string]interface{}:
+            continue
+            break
+        default:
+            return false
+        }
+    }
+    return true
+}
+
+// convert a unix time to time object
+func UnixToTime(i int64) time.Time {
+    return time.Unix(i, 0)
+}
+
+// check whether the value passed is int, float64, float32 or int64
+func IsNumberValue(val interface{}) bool {
+    switch val.(type) {
+    case int, int64, float32, float64:
+        return true
+    default:
+        return false
+    }
+}
+
+// checks whether the value passed is an integer
+func IsInt(val interface{}) bool {
+    switch val.(type) {
+    case int, int64:
+        return true
+    default:
+        return false
+    }
+}
+
+// checks whether the value passed is a json.Number type
+func IsJSONNumber(val interface{}) bool {
+    switch val.(type) {
+    case json.Number:
+        return true
+    default:
+        return false
+    }
+}
+
+// cast int value to float64
+func IntToFloat64(num interface{}) float64 {
+    switch v := num.(type) {
+    case int:
+        return float64(v)
+    case int64:
+        return float64(v)
+    default:
+        panic("failed to cast unsupported type to float64")
+    }
+}
+
+// converts int, float32 and float64 to int64
+func ToInt64(num interface{}) int64 {
+    switch v := num.(type) {
+    case int:
+        return int64(v)
+        break
+    case int64:
+        return v
+        break
+    case float64:
+        return int64(v)
+        break
+    case string:
+        val, _ := strconv.ParseInt(v, 10, 64)
+        return val
+        break
+    default:
+        panic("type is unsupported")
+    }
+    return 0
+}
+
+// get environment variable or return a default value when no set
+func Env(key, defStr string) string {
+    val := os.Getenv(key)
+    if val == "" && defStr != "" {
+        return defStr
+    }
+    return val
+}
+
+// check if a map is empty
+func IsMapEmpty(m map[string]interface{}) bool {
+    return len(GetMapKeys(m)) == 0
+}
+
+// converts int to string
+func IntToString(v int64) string {
+    return fmt.Sprintf("%d", v)
+}
+
+// Given a map, it returns a json string representation of it
+func MapToJSON(m map[string]interface{}) (string, error) {
+    bs, err := json.Marshal(&m)
+    if err != nil {
+        return "", err;
+    }
+    return string(bs), nil;
+}
+
+// Given a json string, it decodes it into a map
+func JSONToMap(jsonStr string) (map[string]interface{}, error) {
+    var data map[string]interface{}
+    d := json.NewDecoder(strings.NewReader(jsonStr))
+    d.UseNumber()
+    if err := d.Decode(&data); err != nil {
+        return make(map[string]interface{}), errors.New("unable to parse json string");
+    }
+    return data, nil
+}
+
+// Get the encoded payload from a JWS token
+func GetJWSPayload(token string) (string, error) {
+    var parts = strings.Split(token, ".")
+    if len(parts) != 3 {
+        return "", errors.New("parameter is not a valid token")
+    }
+    return parts[1], nil
+}
---
+++ utility_test.go
@@ -0,0 +1,210 @@
+package util 
+
+import (
+	"testing"
+	"strings"
+	"time"
+	"encoding/json"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/ellcrys/crypto"
+)
+
+// TestGenerateKeyPair tests that public and private key pairs are set
+func TestGenerateKeyPair(t *testing.T) {
+	keys, err := crypto.GenerateKeyPair()
+	assert.Nil(t, err, "nil not expected")
+	assert.NotNil(t, keys["private_key"], "private key should be set")
+	assert.NotNil(t, keys["public_key"], "public key should be set")
+}
+
+// TestSha1 tests that sha1 hashing a string will return expected hash
+func TestSha1(t *testing.T) {
+	txt := "john"
+	h := Sha1(txt)
+	assert.Equal(t, h, "a51dda7c7ff50b61eaea0444371f4a6a9301e501")
+}
+
+// TestDecodeJSONToMap tests that a json string is successfully decoded to a map
+func TestDecodeJSONToMap(t *testing.T) {
+	var s = `{"name":"john doe"}`
+	m, err := DecodeJSONToMap(s)
+	assert.Nil(t, err)
+	assert.Equal(t, m["name"], "john doe")
+}
+
+// TestReadOneByteAtATime tests that the ReadReader can read one byte at a time
+// and pass the read byte to the passed callback
+func TestReadOneByteAtATime(t *testing.T) {
+	var count = 0
+	text := "hello"
+	reader := strings.NewReader(text)
+	ReadReader(reader, 1, func (err error, bs []byte, done bool) bool {
+		assert.Nil(t, err)
+		if (!done) {
+			assert.Equal(t, string(bs), string(text[count]))
+			count++
+		}
+		return true
+	})
+}
+
+func TestJSONToSliceString(t *testing.T) {
+	var d = `["kennedy", "odion"]`
+	s, err := JSONToSliceString(d)
+	assert.Nil(t, err)
+	assert.Equal(t, len(s), 2)
+	assert.Equal(t, s[0], "kennedy")
+	assert.Equal(t, s[1], "odion")
+}
+
+// TestByteArrToString tests that a byte array is properly converted to string
+func TestByteArrToString(t *testing.T) {
+	var s = ByteArrToString([]byte("hello"))
+	assert.Equal(t, s, "hello")
+}
+
+// TestNewID tests should create an id with 40 characters
+func TestNewID(t *testing.T) {
+	var id = NewID()
+	assert.Equal(t, len(id), 40)
+}
+
+// TestValueNotInStringSlice tests that a string value is not contained in a string slice
+func TestValueNotInStringSlice(t *testing.T) {
+	var ss = []string{"john", "doe"}
+	var r = InStringSlice(ss, "jane")
+	assert.Equal(t, r, false)
+}
+
+
+// TestValueInStringSlice tests that a string value is contained in a string slice or not
+func TestValueInStringSlice(t *testing.T) {
+	var ss = []string{"john", "doe"}
+	assert.Equal(t, InStringSlice(ss, "john"), true)
+	assert.Equal(t, InStringSlice(ss, "jane"), false)
+}
+
+// TestGetMapKeys tests that GetMapKeys will return a list of all keys it contains
+func TestGetMapKeys(t *testing.T) {
+	var keys = GetMapKeys(map[string]interface{}{
+		"key1": "0",
+		"key2": "1",
+	})
+	r := InStringSlice(keys, "key1")
+	r2 := InStringSlice(keys, "key2")
+	assert.Equal(t, len(keys), 2)
+	assert.Equal(t, r, true)
+	assert.Equal(t, r2, true)
+}
+
+// TestHasKey tests that a key exist or doesn't exist in a map
+func TestHasKey(t *testing.T) {
+	var m = map[string]interface{}{
+		"stuff_a": 2,
+		"stuff_b": 3,
+	}
+	assert.Equal(t, HasKey(m, "stuff_b"), true)
+	assert.Equal(t, HasKey(m, "stuff_a"), true)
+	assert.Equal(t, HasKey(m, "stuff_c"), false)
+}
+
+// TestIsStringValue tests that a variable holds a string value or not
+func TestIsStringValue(t *testing.T) {
+	assert.Equal(t, IsStringValue("lorem"), true)
+	assert.Equal(t, IsStringValue(20), false)
+}
+
+// TestIsMapOfAny tests that a variable's value type is a map of any type as value
+func TestIsMapOfAny(t *testing.T) {
+	var m = make(map[string]interface{})
+	assert.Equal(t, IsMapOfAny(m), true)
+	assert.Equal(t, IsMapOfAny(10), false)
+}
+
+// TestContainsOnlyMapType tests that a variable's value type is a slice containing only map type
+func TestContainsOnlyMapType(t *testing.T) {
+	s := []interface{}{
+		map[string]interface{}{ "a": "b" },
+	}
+	assert.Equal(t, true, ContainsOnlyMapType(s))
+}
+
+func TestUnixToTime(t *testing.T) {
+	expected := time.Now().Unix()
+	tm := UnixToTime(expected)
+	assert.Equal(t, expected, tm.Unix())
+}
+
+func TestIsNumberValue(t *testing.T) {
+	assert.Equal(t, IsNumberValue(1), true)
+	assert.Equal(t, IsNumberValue(1.5), true)
+	assert.Equal(t, IsNumberValue("abc"), false)
+	assert.Equal(t, IsNumberValue([]string{}), false)
+}
+
+func TestIsInt(t *testing.T) {
+	assert.Equal(t, IsInt(1), true)
+	assert.Equal(t, IsInt(int64(1)), true)
+	assert.Equal(t, IsInt(1.5), false)
+	assert.Equal(t, IsInt("1"), false)
+	assert.Equal(t, IsInt([]string{}), false)
+}
+
+func TestIsJSONNumber(t *testing.T) {
+	var jn json.Number
+	jn = "1"
+	assert.Equal(t, IsJSONNumber(jn), true)
+	assert.Equal(t, IsJSONNumber(1), false)
+}
+
+func TestIntToFloat64(t *testing.T) {
+	expected := interface{}(IntToFloat64(1))
+	switch expected.(type) {
+	case float64:
+		assert.Equal(t, expected, 1.0)
+		break
+	default:
+		assert.Fail(t, "expected value type must be float64")
+	}
+}
+
+func TestIsMapEmpty(t *testing.T) {
+	expected := IsMapEmpty(map[string]interface{}{})
+	expected2 := IsMapEmpty(map[string]interface{}{ "a": "b" })
+	assert.Equal(t, expected, true)
+	assert.Equal(t, expected2, false)
+}
+
+func TestIntToString(t *testing.T) {
+	assert.Equal(t, IntToString(1), "1")
+}
+
+func TestMapToJSON(t *testing.T) {
+	m := map[string]interface{}{ "a": "b" }
+	v, err := MapToJSON(m)
+	assert.Nil(t, err)
+	assert.Equal(t, v, `{"a":"b"}`)
+}
+
+func TestJSONToMap(t *testing.T) {
+	s := `{"a":"b"}`
+	expected := map[string]interface{}{ "a": "b" }
+	m, err := JSONToMap(s)
+	assert.Nil(t, err)
+	assert.Exactly(t, m, expected)
+}
+
+func TestGetJWSPayload(t *testing.T) {
+	var validJWS = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyZXNzX2lkIjoiNTZmZDBiZTNmNjhkYWE0MGEzMDAwMDAyIiwiYXNzZXRfaWRzIjpbIjRiYTM0NDNiNjE1Nzc1M2ZiNjc4YTU5M2U1ZDc2ODRjODRkMWIyMDciXSwiZW5kcG9pbnQiOiIvdjEvYWRkcmVzc2VzLzU2ZmQ4YTE4ZjY4ZGFhNWMzZDAwMDAwMS9kZXBvc2l0IiwiaWF0IjoxNDU5Njc4NjkyfQ.hKWb7KtMCTf4VI5CCkJMS1r33DDDmyKiwuKU_DVPl8HRg152mUtqzRrdCUC2VXlryBJWtrXwZWu0NcAnJcraTSJ8IW60Lgi6Bd2pGTp-eDSkeR8kDYG4sMBMHKjm-fJaO0sYlEdgeZSp9bpV24417na_e6Ct7mRkNverQo0bL9M`
+	var expected = strings.Split(validJWS, ".")
+	p, err := GetJWSPayload(validJWS)
+	assert.Nil(t, err)
+	assert.Equal(t, p, expected[1])
+}
+
+func TestGetJWSPayloadWithInvalidJWSToken(t *testing.T) {
+	var invalidJWS = `aa.aa`
+	_, err := GetJWSPayload(invalidJWS)
+	assert.NotNil(t, err)
+}
